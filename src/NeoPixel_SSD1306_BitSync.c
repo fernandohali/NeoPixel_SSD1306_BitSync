c8cf35ac7a5019a11f8e15215fa705e91c2f5a21
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2025-02-09 12:29:33 -0300

    Na main principal foi feito a chamada das funções.
    Na main principal foi feito a chamada da funções que estão em outros arquivos e também foi colocado o scanf para digitar e aparecer os valores na matriz e no display.

--- src/NeoPixel_SSD1306_BitSync.c
+++ src/NeoPixel_SSD1306_BitSync.c
@@ -7,18 +7,38 @@
 #include "inc/font.h"
 #include "display/display.h"
 
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "ws2812.pio.h"
+
+// Chamando as funções organizadas em pastas
+#include "led/led.h"
+#include "buttons/buttons.h"
+#include "matriz_Numeros/matriz_Numeros.h"
+
 // Definição do display
 ssd1306_t ssd;
 
+PIO pio = pio0; // Definição global para evitar reinicialização contínua
+int sm = 0;
+uint offset;
+
 int main()
 {
-    char valor;
+    char valor = '\0';
 
     // Inicializa a comunicação serial USB
     stdio_init_all(); // Isso é necessário para usar o printf via USB
 
+    // Inicializa os LEDs e os botões
+    init_buttons();
+
     // Inicializa o display
     init_display(&ssd);
+
+    // Inicializa a comunicação WS2812 apenas uma vez
+    uint offset = pio_add_program(pio, &ws2812_program);
+    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
 
     printf("Iniciando...\n");
 
